7c21d6741e72a5643dce4fc767d35e2e1823917e
RayaanJIrani <rayaanjirani@gmail.com>
2022-01-22 16:42:35 -0600

    Added basic functionality to take in directory and to complie and execute all of the cpp files in it

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import os
+from datetime import datetime
 
 def main():
-    print('Hello World!')
+    os.chdir('/Users/rayaanirani/Desktop/OSPlayground')
+    cpp_list = []
+    for file in os.listdir():
+        if os.path.splitext(file)[1] == '.cpp':
+            cpp_list.append(file)
 
+    for file in cpp_list:
+        os.system('g++ -std=c++11' + ' ' + file)
+        os.system('./a.out')
+        os.system('rm a.out')
 
 if __name__ == '__main__':
     main()
