8aabeea38e22984a848486a1bb29058907a89a20
RayaanJIrani <rayaanjirani@gmail.com>
2022-01-22 00:20:31 -0600

    Added main and basicCode.cpp

---
+++ basicCode.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    std::cout << "Enter a number: ";
+    int number;
+    std::cin >> number;
+    std::cout << "Twice that is " << number * 2 << std::endl;
+    return 0;
+}
